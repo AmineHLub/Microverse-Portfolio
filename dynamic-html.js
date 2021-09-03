5626cee2676a338d4c12eb2860cbcccbb152ae11
AmineHLub <Amino.bhl@gmail.com>
2021-09-03 01:55:57 +0100

    Fixes allover Desktop modal CSS
    Add more functionality and features(blur/overflow/change buttons to a tag)

--- dynamic-html.js
+++ dynamic-html.js
@@ -4,7 +4,7 @@ const arr = [
   {
     close: './project-images/Icon.png',
     logo: './project-images/one.png',
-    title: 'Keeping track of hundreds of components',
+    title: 'Random name for project 1',
     tech: ['Ruby on rails', 'css', 'JavaScript', 'html'],
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum recusandae accusamus minima et officiis vel, unde cum saepe officia, iusto eaque doloremque commodi quisquam rem nisi error illum ipsa repellat!',
     id: '1',
@@ -14,7 +14,7 @@ const arr = [
   {
     close: './project-images/Icon.png',
     logo: './project-images/two.png',
-    title: 'Keeping track of hundreds of components',
+    title: 'Random name for project 2',
     tech: ['ReactJS', 'Flask', 'Html'],
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum recusandae accusamus minima et officiis vel, unde cum saepe officia, iusto eaque doloremque commodi quisquam rem nisi error illum ipsa repellat!',
     id: '2',
@@ -24,7 +24,7 @@ const arr = [
   {
     close: './project-images/Icon.png',
     logo: './project-images/three.png',
-    title: 'Project 3',
+    title: 'Random name for project 3',
     tech: ['Ruby on rails', 'css', 'JavaScript', 'html'],
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum recusandae accusamus minima et officiis vel, unde cum saepe officia, iusto eaque doloremque commodi quisquam rem nisi error illum ipsa repellat!',
     id: '3',
@@ -34,7 +34,7 @@ const arr = [
   {
     close: './project-images/Icon.png',
     logo: './project-images/four.png',
-    title: 'Project 4',
+    title: 'Random name for project 4',
     tech: ['css', 'JavaScript', 'html'],
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum recusandae accusamus minima et officiis vel, unde cum saepe officia, iusto eaque doloremque commodi quisquam rem nisi error illum ipsa repellat!',
     id: '4',
@@ -44,7 +44,7 @@ const arr = [
   {
     close: './project-images/Icon.png',
     logo: './project-images/five.png',
-    title: 'Project 5',
+    title: 'Random name for project 5',
     tech: ['Ruby on rails', 'bootstrap', 'html', 'css'],
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum recusandae accusamus minima et officiis vel, unde cum saepe officia, iusto eaque doloremque commodi quisquam rem nisi error illum ipsa repellat!',
     id: '5',
@@ -55,7 +55,7 @@ const arr = [
   {
     close: './project-images/Icon.png',
     logo: './project-images/six.png',
-    title: 'Project 6',
+    title: 'Random name for project 6',
     tech: ['css', 'JavaScript', 'html'],
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum recusandae accusamus minima et officiis vel, unde cum saepe officia, iusto eaque doloremque commodi quisquam rem nisi error illum ipsa repellat!',
     id: '6',
@@ -92,9 +92,12 @@ for (let j = 8; j < 8 + iterationJ; j += 1) {
 
 // eslint-disable-next-line no-unused-vars
 function closeModal() {
-  document.querySelector('.modal-container').style.display = 'none';
+  document.querySelector('body').firstChild.remove();
+  document.querySelector('*').style.overflow = 'auto';
+  document.querySelector('#whole-wrapper').classList.remove('activeblur');
 }
 
+// eslint-disable-next-line no-unused-vars
 function modalCreation(number) {
   // get rid of the previous Modal
   const divContExists = document.getElementsByClassName('modal-container');
@@ -107,7 +110,6 @@ function modalCreation(number) {
   const closeX = document.createElement('img');
   const closeXLink = document.createElement('a');
   const ul = document.createElement('ul');
-  const logoImg = document.createElement('img');
   const divDesk = document.createElement('div');
   const header = document.createElement('h1');
   const technology = document.createElement('div');
@@ -117,7 +119,6 @@ function modalCreation(number) {
   divCont.classList.add('modal-container');
   firstCont.classList.add('child-modal-container');
   closeX.classList.add('modal-closeX');
-  logoImg.classList.add('modal-logo');
   divDesk.classList.add('modal-desk');
   header.classList.add('modal-header');
   technology.classList.add('modal-langs');
@@ -127,15 +128,14 @@ function modalCreation(number) {
   ul.classList.add('modal-ul');
   // Logic & Section creation
   modalSection.appendChild(divCont);
-  divCont.appendChild(logoImg);
   divCont.appendChild(firstCont);
+  firstCont.appendChild(buttonsCont);
   firstCont.appendChild(closeXLink);
   closeXLink.appendChild(closeX);
   divDesk.appendChild(header);
-  divDesk.appendChild(buttonsCont);
-  closeXLink.href = '#';
   closeX.src = arr[number].close;
-  logoImg.src = arr[number].logo;
+  document.querySelector('#whole-wrapper').classList.add('activeblur');
+  document.querySelector('*').style.overflow = 'hidden';
 
   // Background-Image
   divCont.appendChild(header);
@@ -150,12 +150,13 @@ function modalCreation(number) {
   description.innerText = arr[number].description;
   divCont.appendChild(buttonsCont);
   for (let i = 0; i < 2; i += 1) {
-    (buttonsCont).appendChild(document.createElement('button'));
+    (buttonsCont).appendChild(document.createElement('a'));
   }
-  // document.querySelectorAll('.modal-buttonsCont > button')[0].innerText = 'See Live';
-  document.querySelectorAll('.modal-buttonsCont > button')[0].innerHTML = "See Live <img src='./src/seelive.svg'>";
-  document.querySelectorAll('.modal-buttonsCont > button')[1].innerHTML= "Source <img src='./src/source.png'>";
-
+  document.querySelectorAll('.modal-buttonsCont > a')[0].classList.add('see-live');
+  document.querySelectorAll('.modal-buttonsCont > a')[1].classList.add('source');
+  document.querySelector('.child-modal-container').style.backgroundImage = `url(${arr[number].logo})`;
+  document.querySelectorAll('.modal-buttonsCont a')[0].href = arr[number].link;
+  document.querySelectorAll('.modal-buttonsCont a')[1].href = arr[number].source;
   const closefun = document.querySelector('.close-modal');
   closefun.addEventListener('click', closeModal);
 }
